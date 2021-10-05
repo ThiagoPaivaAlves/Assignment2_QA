42b3e93fb8bba0895f0d99f7fe98d1aefc7f3054
ThiagoPaivaAlves <88196831+ThiagoPaivaAlves@users.noreply.github.com>
2021-10-05 10:26:39 -0300

    Created Rectangle code

--- Assignment2_QA/Rectangle.cs
+++ Assignment2_QA/Rectangle.cs
@@ -8,5 +8,21 @@ namespace Assignment2_QA
 {
     class Rectangle
     {
+        private int width;
+        private int height;
+
+        public Rectangle()
+        {
+            this.width = 1;
+            this.height = 1;
+        }
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Width { get => width; set => width = value; }
+        public int Height { get => height; set => height = value; }
     }
 }
