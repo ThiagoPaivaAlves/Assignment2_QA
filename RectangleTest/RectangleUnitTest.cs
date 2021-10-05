f9a1632a06ac05f4f5806f4dae88a2065754479c
ThiagoPaivaAlves <88196831+ThiagoPaivaAlves@users.noreply.github.com>
2021-10-05 20:57:32 -0300

    Changed the default name of the class to a more readable name

--- RectangleTest/RectangleUnitTest.cs
+++ RectangleTest/RectangleUnitTest.cs
@@ -5,7 +5,7 @@ using System;
 namespace RectangleTest
 {
     [TestClass]
-    public class UnitTest1
+    public class RectangleUnitTest
     {
         [TestMethod]
         public void TestDefaultConstructor()
